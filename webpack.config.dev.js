d2404ee366164448c2f0ded2523e6a85fd49c326
ptoninato <pmtoninato@gmail.com>
2020-06-09 14:37:21 -0500

    add webpack?

---
+++ webpack.config.dev.js
@@ -0,0 +1,22 @@
+import path from 'path';
+
+export default {
+  mode: "development",
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  // module: {
+  //   loaders: [
+  //     {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+  //     {test: /\.css$/, loaders: ['style','css']}
+  //   ]
+  // }
+}
